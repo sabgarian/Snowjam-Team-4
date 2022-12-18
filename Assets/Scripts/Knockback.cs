d30354b97b24708602a245f8d2e20b64583d4b31
BrandTruong <btruong100381@gmail.com>
2022-12-18 15:17:18 -0800

    fixed level

--- Assets/Scripts/Knockback.cs
+++ Assets/Scripts/Knockback.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Knockback : MonoBehaviour
 {
-    [SerializeField] private float force = 5f, delay = 0.15f;
+    [SerializeField] private float force = 150f;
     [SerializeField] private GameObject player;
     // Start is called before the first frame update
     void Start()
@@ -17,12 +17,7 @@ public class Knockback : MonoBehaviour
         Vector2 direction = (player.transform.position - transform.position).normalized;
         Debug.Log(direction);
         player.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
-        StartCoroutine(Reset());
+
     }
 
-    IEnumerator Reset()
-    {
-        yield return new WaitForSeconds(delay);
-        player.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-    }
 }
